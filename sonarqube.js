afe5db434f25fd0e1059dc122d938c9e735ce265
Ashraf Hofny <ashraf.hofny@blink22.com>
2022-02-08 20:01:28 +0200

    feature: continue setup

--- sonarqube.js
+++ sonarqube.js
@@ -11,12 +11,13 @@ scanner(
       "sonar.projectKey": "node-ts-conventions",
       "sonar.projectVersion": "1.0.0",
       "sonar.login": process.env.SONAR_LOGIN,
-      "sonar.sources": ".",
+      "sonar.sources": "src",
+      "sonar.tests": "tests",
       "sonar.test.inclusions": "**/*.test.*",
       "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
       "sonar.testExecutionReportPaths": "test-report.xml",
       "sonar.coverage.exclusions":
-        "**/*.spec.js,**/*.spec.ts,**/*.mock.js,**/*.mock.ts,node_modules/*,coverage/lcov-report/*",
+        "**/*.test.*,**/*.spec.js,**/*.spec.ts,**/*.mock.js,**/*.mock.ts,node_modules/*,coverage/lcov-report/*",
     },
   },
   () => process.exit()
