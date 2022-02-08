fb62a9a196c702ac64d769db5809323f180b7ba6
Ashraf Hofny <ashraf.hofny@blink22.com>
2022-02-08 18:57:47 +0200

    feature: continue setup

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,9 @@ module.exports = {
   moduleFileExtensions: ["js", "ts"],
   testResultsProcessor: "jest-sonar-reporter",
   transform: {
-    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    "^.+\\.ts$": "**/node_modules/ts-jest/preprocessor.js",
   },
-  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  testMatch: ["**/tests/**/*.test.ts"],
   collectCoverage: true,
   coverageThreshold: {
     global: {
