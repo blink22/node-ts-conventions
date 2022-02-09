0b1711597086d5700c7ff0d4d3a6816e08289cc3
amr-beshr <amr.beshr@blink22>
2022-02-09 16:44:42 +0200

    setup: enhance unit tests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 export function fibonacci(num: number): number | null {
   if (num < 0) {
+    console.error(`Error: the number ${num} is a negative value`);
     return null;
   }
 
-  let a = 42, b = 0, temp;
+  let a = 42,
+    b = 0,
+    temp;
   a = 1;
 
-  while (num >= 0){
+  while (num >= 0) {
     temp = a;
     a = a + b;
     b = temp;
     num--;
   }
 
+  console.log(`return value is ${b}`);
   return b;
 }
-console.log("Hello");
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,26 @@
-import { fibonacci } from "../src/index"
+import { fibonacci } from "../src/index";
 
-it("should return '1' for 1", () => {
-  expect(fibonacci(1)).toBe(1);
-})
+describe("fibonacci ", () => {
+  const consoleLogSpy = jest.spyOn(console, "log");
+  const consoleErrorSpy = jest.spyOn(console, "error");
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should return '1' for 1", () => {
+    expect(fibonacci(1)).toBe(1);
+    expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+    expect(consoleLogSpy).toHaveBeenCalledWith("return value is 1");
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it("should return null for -1", () => {
+    expect(fibonacci(-1)).toBe(null);
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      "Error: the number -1 is a negative value"
+    );
+    expect(consoleLogSpy).toHaveBeenCalledTimes(0);
+  });
+});
