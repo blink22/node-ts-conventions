8d34f3e755a5c0db87482c609e8e96d5d1228110
Ashraf Hofny <ashraf.hofny@blink22.com>
2022-03-16 07:02:28 +0200

    feature: add sonar project status report

--- sonarqube.js
+++ sonarqube.js
@@ -1,14 +1,47 @@
 const scanner = require("sonarqube-scanner");
 const dotenv = require("dotenv");
+const { default: axios } = require("axios");
 
 dotenv.config();
+const projectKey = "node-ts-conventions";
+
+const checkProjectStatus = async () => {
+  const projectStatus = await axios
+    .get(`${process.env.SONAR_URL}/api/qualitygates/project_status`, {
+      params: {
+        projectKey,
+      },
+      auth: {
+        username: process.env.SONAR_LOGIN,
+        password: "",
+      },
+    })
+    .then((response) => {
+      return response?.data?.projectStatus;
+    })
+    .catch((error) => {
+      console.log(`${error?.name}: ${error?.message}`);
+
+      return null;
+    });
+
+  if (!projectStatus) {
+    throw new Error("Failed to fetch Project Status");
+  }
+
+  console.table(projectStatus.conditions);
+  
+  if (projectStatus.status !== "OK") {
+    throw new Error("QualityGate Failed!");
+  }
+};
 
 scanner(
   {
     // this example uses local instance of SQ
     serverUrl: process.env.SONAR_URL,
     options: {
-      "sonar.projectKey": "node-ts-conventions",
+      "sonar.projectKey": projectKey,
       "sonar.projectVersion": "1.0.0",
       "sonar.login": process.env.SONAR_LOGIN,
       "sonar.sources": "src",
@@ -20,5 +53,5 @@ scanner(
         "**/*.test.*,**/*.spec.js,**/*.spec.ts,**/*.mock.js,**/*.mock.ts,node_modules/*,coverage/lcov-report/*",
     },
   },
-  () => process.exit()
+  checkProjectStatus
 );
